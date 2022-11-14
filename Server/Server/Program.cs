a19321a7d9950eec6b5a1d4d2792f2ff1ec88f2e
Al3To <aletommy238@gmail.com>
2022-11-14 20:23:22 +0100

    Aggiornato il server, introdotta funzionalità di base per il login lato server

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -4,6 +4,7 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 using System.Runtime.CompilerServices;
+using System.Security.Principal;
 
 IPAddress ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
 IPEndPoint remoteEP = new IPEndPoint(ipAddress, 8888);
@@ -19,10 +20,10 @@ while (!temp)
     {
         case "start":
             temp = true;
-            startServer();
+            StartServer();
             break;
         case "help":
-            helpCommand();
+            HelpCommand();
             break;
         default:
             Console.WriteLine("Comando errato, digitare 'help' per la lista dei comandi" + Environment.NewLine);
@@ -30,42 +31,87 @@ while (!temp)
     }
 }
 
-void helpCommand()
+void HelpCommand()
 {
     Console.WriteLine("start: Avvia il server" + Environment.NewLine);
 }
 
-void startServer()
+void StartServer()
 {
     _socket.Listen(100);
-    startThread();
+    StartThread();
 }
 
-void startThread()
+void StartThread()
 {
-    Thread t = new Thread(game);
+    Thread t = new Thread(Game);
     t.Start();
 }
 
-void game()
+void Game()
 {
     Thread.Sleep(1000);
     Socket handler;
     handler = _socket.Accept();
     Console.WriteLine("Someone connected to the server");
-    startThread();
+    players++;
+    StartThread();
     while (true)
     {
         string data = null;
         byte[] bytes = new byte[1024];
+        byte[] toSend = null;
         if (SocketExtensions.IsConnected(handler))
         {
             try
             {
                 int bytesRec = handler.Receive(bytes);
                 data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                if (data.EndsWith("/o/"))
+                if (data.EndsWith("/s/"))
+                {
+                    data = data.Remove(data.Length - 1);
+                    data = data.Remove(data.Length - 1);
+                    data = data.Remove(data.Length - 1);
                     Console.WriteLine(data);
+                    switch (data)
+                    {
+                        case "Login":
+                            List<string> username = new List<string>();
+                            List<string> password = new List<string>();
+                            List<string> role = new List<string>();
+                            string[] temp;
+                            int n = 0;
+                            bytesRec = handler.Receive(bytes);
+                            data = null;
+                            data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                            string[] credentials = data.Split(";");
+                            Console.Write(credentials[0] + "\n");
+                            Console.Write(credentials[1]);
+                            foreach (string line in File.ReadLines(@"../../../../files/users.txt"))
+                            {
+                                temp = line.Split(";");
+                                username.Add(temp[0]);
+                                password.Add(temp[1]);
+                                role.Add(temp[2]);
+                                n++;
+                            }
+
+                            for (int j = 0; j < username.Count; ++j)
+                            {
+                                if (credentials[0] == username[j] && credentials[1] == password[j])
+                                {
+                                    toSend = Encoding.ASCII.GetBytes(role[j] + "/c/");
+                                    handler.Send(toSend);
+                                    Console.Write(toSend);
+                                }
+                                Console.Write(j);
+
+                            }
+                            Console.Write("finito");
+                            break;
+                    }
+
+                }
 
             }
             catch (Exception e)
@@ -75,7 +121,9 @@ void game()
         }
         else
         {
+            players--;
             Thread.Sleep(Timeout.Infinite);
+
         }
 
     }
